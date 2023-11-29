b04af71643eb3c425ebd2dcf996306241927f360
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-11-29 19:14:13 +0600

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,28 @@
+let btnCatalog = document.querySelector(".header__catalog");
+let blockCatalog = document.querySelector(".catalog__menu-block");
+btnCatalog.addEventListener("click", () => {
+  if (blockCatalog.style.display === "block") {
+    blockCatalog.style.display = "none";
+  } else {
+    blockCatalog.style.display = "block";
+  }
+});
+
+let btnMapSpb = document.querySelector(".map-btn-spb");
+let btnMapMoscol = document.querySelector(".map-btn");
+let blockMoscol = document.querySelector(".block-maps-flex");
+let btnActive = document.querySelector(".map-btn-active");
+let blockSpb = document.querySelector(".block-maps-flex-Spb");
+btnMapSpb.addEventListener("click", () => {
+  blockSpb.style.display = "block";
+  blockMoscol.style.display = "none";
+  btnMapSpb.classList.add("map-btn-active");
+  btnMapMoscol.classList.remove("map-btn-active");
+});
+
+btnMapMoscol.addEventListener("click", () => {
+  blockSpb.style.display = "none";
+  blockMoscol.style.display = "block";
+  btnMapMoscol.classList.add("map-btn-active");
+  btnMapSpb.classList.remove("map-btn-active");
+});
