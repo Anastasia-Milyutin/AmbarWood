dad927b4887b6711dbce61738382c06b9e7ffa11
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-12-02 19:45:14 +0600

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,38 @@
+let btnCatalog = document.querySelector('.header__catalog')
+let blockCatalog = document.querySelector('.catalog__menu-block')
+btnCatalog.addEventListener('click', () => {
+  if (blockCatalog.style.display === "block") {
+    blockCatalog.style.display = "none";
+  } else {
+    blockCatalog.style.display = "block";
+  }
+}
+)
+
+const btnRight = document.querySelector('.work-arrow-btn-right');
+btnRight.addEventListener('click', moveRight);
+
+const btnLeft = document.querySelector('.work-arrow-btn-left');
+btnLeft.addEventListener('click', moveLeft);
+
+const dots = document.querySelectorAll('.btn-nomber');
+
+let activeIndex = 0;
+
+function moveRight() {
+  dots[activeIndex].classList.remove('btn-nomber-active');
+  activeIndex++;
+  if (activeIndex === dots.length) {
+    activeIndex = 0;
+  }
+  dots[activeIndex].classList.add('btn-nomber-active');
+}
+
+function moveLeft() {
+  dots[activeIndex].classList.remove('btn-nomber-active');
+  activeIndex--;
+  if (activeIndex < 0) {
+    activeIndex = dots.length - 1;
+  }
+  dots[activeIndex].classList.add('btn-nomber-active');
+}
